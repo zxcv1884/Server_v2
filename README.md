33547b0fdcf00ab3d042b739b515fe0a34f381cc
zxcv1884 <zxcv1884@gmail.com>
2019-01-23 22:39:00 +0800

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Server_v2
+## Server_v2
+
+#運用Node.js將中央氣象局全臺3天天氣預報 & Google新聞自動填入資料庫。
+
+-Create database 'server'
+-Import server.sql
+-Edit New.js & Weathers.js => mysql.createConnection
+-npm install
+-npm start
---
+++ server.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機: 127.0.0.1
+-- 產生時間： 2019 年 01 月 23 日 15:29
+-- 伺服器版本: 10.1.32-MariaDB
+-- PHP 版本： 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `server`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `news`
+--
+
+CREATE TABLE `news` (
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `title` text NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `click` int(11) DEFAULT NULL,
+  `category` varchar(255) NOT NULL,
+  `ID` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `weathers`
+--
+
+CREATE TABLE `weathers` (
+  `縣市` varchar(255) NOT NULL,
+  `鄉鎮` varchar(255) NOT NULL,
+  `時間` datetime NOT NULL,
+  `天氣現象` text,
+  `天氣現象編號` int(100) DEFAULT NULL,
+  `降雨機率` int(100) DEFAULT NULL,
+  `溫度` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `url` (`url`);
+
+--
+-- 資料表索引 `weathers`
+--
+ALTER TABLE `weathers`
+  ADD UNIQUE KEY `鄉鎮` (`鄉鎮`,`時間`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `news`
+--
+ALTER TABLE `news`
+  MODIFY `ID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34251;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
