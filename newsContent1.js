b04210c3c3288b53178be51b4e7f59ce5369c2d5
zxcv1884 <zxcv1884@gmail.com>
2019-02-16 23:25:30 +0800

    Fix memory burst
    Use async.mapLimit to fix memory burst.

---
+++ newsContent1.js
@@ -0,0 +1,60 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const mysql = require('mysql');
+const async = require("async");
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "nayuyu1884",
+    database: "server"
+});
+
+function run() {
+    setTimeout(function () {
+        let sql = "SELECT url FROM news WHERE `realUrl` IS NULL ";
+        let count = 0;
+        con.query(sql, function (err, rows, result) {
+            let news;
+            let fakeUrl = [];
+            rows.forEach(function (row) {
+                fakeUrl.push(row.url)
+            });
+            async.mapLimit(fakeUrl, 10, function (url, callback) {
+                fetchUrl(url, callback);
+            }, function (err, result) {
+                sql = "DELETE FROM `news` WHERE `realUrl` = '//www.google.com'";
+                con.query(sql, function (err, rows, result) {
+                });
+                console.log(count);
+            });
+
+            function fetchUrl(url, callback) {
+                request({
+                    url: url,
+                    method: "GET"
+                }, function (error, response, body) {
+                    if (error || !body) {
+                        return;
+                    }
+                    let news = analyze(body);
+                    sql = "UPDATE `news` SET `realUrl`='" + news + "' WHERE `news`.`url` = '" + url + "'";
+                    con.query(sql, function (err, rows, result) {
+                    });
+                    count++;
+                    callback(null, count);
+                })
+            }
+
+            function analyze(body) {
+                const $ = cheerio.load(body);
+                $('body a').each(function () {
+                    news = $(this).attr('href');
+                });
+                return news;
+            }
+        })
+    },);
+}
+
+run();
+setInterval(run, 1000 * 60 * 30);
--- newsContent2.js
+++ newsContent2.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 const mysql = require('mysql');
+const async = require("async");
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -10,50 +11,75 @@ const con = mysql.createConnection({
 
 function run() {
     setTimeout(function () {
-        let sql = "SELECT RealUrl,ID FROM news WHERE `article` IS NULL";
+        let sql = "SELECT realUrl FROM news WHERE `article` IS NULL";
+        let count = 0;
         con.query(sql, function (err, rows, result) {
+            let realUrl = [];
             rows.forEach(function (row) {
+                realUrl.push(row.realUrl)
+            });
+            console.log(realUrl);
+            async.mapLimit(realUrl, 10, function (url, callback) {
+                fetchUrl(url, callback);
+            }, function (err, result) {
+                sql = "DELETE FROM `news` WHERE `article` = '' or `article` IS NULL" ;
+                con.query(sql, function (err, rows, result) {
+                });
+                console.log(count);
+            });
+
+            function fetchUrl(url, callback) {
                 request({
-                    url: row.RealUrl,
+                    url: url,
                     method: "GET"
                 }, function (error, response, body) {
-                    if (error || !body) {
+                    if (error || !body || response.statusCode !== 200) {
                         return;
                     }
-                    const $ = cheerio.load(body);
-                    let news = "";
-                    if (!row.RealUrl.includes('apple')) {
-                        $('p').each(function () {
-                            if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
-                                news += $(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, "") + '<br>';
-                            }
-                        });
+                    let news = analyze(body, url);
+                    if(news===""){
+                        sql = "DELETE FROM `news` WHERE `realUrl` = '"+ url +"'";
+                    }else {
+                    sql = "UPDATE `news` SET `article`=\"" + news + "\" WHERE `news`.`realUrl` = '" + url + "'";
                     }
-                    if (row.RealUrl.includes('appledaily')) {
-                        $('article article p').each(function () {
-                            if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
-                                news += $(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, "");
-                            }
-                        });
-                    }
-                    if (news === "") {
-                        $('.h7').each(function () {
-                            if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
-                                news += ($(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, ""));
-                            }
-                        });
-                    }
-                    // if (row.RealUrl.includes('hk')||row.RealUrl.includes('applealmond') ||row.RealUrl.includes('gnn.gamer') || row.RealUrl.includes('stock.yahoo.com')||row.RealUrl.includes('4gamers.com')||row.RealUrl.includes('techbang.com')||row.RealUrl.includes('news.pts.org.tw')||row.RealUrl.includes('thenewslens')) {
-                    sql = "DELETE FROM `news` WHERE `article` = ''";
+                    console.log(sql);
                     con.query(sql, function (err, rows, result) {
                     });
-                    sql = "UPDATE `news` SET `article`='" + news + "' WHERE `news`.`ID` = " + row.ID;
-                    con.query(sql, function (err, rows, result) {
+                    count++;
+                    console.log(count);
+                    callback(null, count);
+                })
+            }
+
+            function analyze(body, url) {
+                const $ = cheerio.load(body);
+                let news = "";
+                if (!url.includes('apple')) {
+                    $('p').each(function () {
+                        if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
+                            news += $(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, "") + '<br>';
+                        }
                     });
-                });
-            });
-        });
-    }, 1000 * 60)
+                }
+                if (url.includes('appledaily')) {
+                    $('article article p').each(function () {
+                        if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
+                            news += $(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, "");
+                        }
+                    });
+                }
+                if (news === "") {
+                    $('.h7').each(function () {
+                        if ($(this).text() !== "" && $(this).text().includes("。") && !($(this).text().includes("]※")) && !($(this).text().includes("圖／")) && !($(this).text().includes("圖/"))) {
+                            news += ($(this).text().replace(/\t/g, "").replace(/\n/g, "<br>").replace(/ /g, ""));
+                        }
+                    });
+                }
+                return news;
+            }
+        })
+    })
 }
+
 run();
-setInterval(run, 1000 * 60 * 30);
+// setInterval(run, 1000 * 60 * 30);
